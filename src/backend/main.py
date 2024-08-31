82ee2f369cce4467918e503ec03351aeb73fac4f
rafaelarojas <rafaelacrisr@gmail.com>
2024-08-31 18:14:00 -0300

    feat: init integration backend & model

--- src/backend/main.py
+++ src/backend/main.py
@@ -2,44 +2,46 @@ from fastapi import FastAPI, UploadFile, Depends, HTTPException
 import pandas as pd
 import torch
 import torch.nn as nn
-from sqlalchemy import create_engine, Column, Integer, String, Float, text
+from sqlalchemy import create_engine, Column, String, Float, Integer, text
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker, Session
 import random
+import uuid
 import time
 from datetime import datetime, timedelta
-import uuid
-import numpy as np
 
+# Configurações do banco de dados
 DATABASE_URL = "postgresql://postgres:SENHA@localhost:5432/fillmore"
-
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
-# Define a SQLAlchemy model to save the data
+# Definição do modelo SQLAlchemy para salvar os dados
 class Prediction(Base):
-    __tablename__ = "teste"
-
+    __tablename__ = "predictions"
     ID = Column(String, primary_key=True, index=True)
     KNR = Column(String)
     Date = Column(String)
-    Feature1 = Column(Float)
-    Feature2 = Column(Float)
-    Feature3 = Column(Float)
+    unique_names = Column(Float)
+    status_10_1 = Column(Float)
+    status_10_2 = Column(Float)
+    status_10_718 = Column(Float)
+    status_13_1 = Column(Float)
+    status_13_2 = Column(Float)
+    status_13_718 = Column(Float)
     Prediction_result = Column(Integer)
     Real_result = Column(Integer)
 
-# Create the table in the database
+# Criação da tabela no banco de dados
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
 
-# Load the PyTorch model
+# Carregamento do modelo PyTorch
 # model = torch.load('model.pt')
-# model.eval()  # Set the model to evaluation mode
+# model.eval()  # Configura o modelo para o modo de avaliação
 
-# Dependency to get the DB session
+# Dependência para obter a sessão do DB
 def get_db():
     db = SessionLocal()
     try:
@@ -47,30 +49,17 @@ def get_db():
     finally:
         db.close()
 
-# def call_ai(df: pd.DataFrame):
-#     # Convert the DataFrame to a tensor
-#     input_data = torch.tensor(df.values, dtype=torch.float32)
-    
-#     # Run the model prediction
-#     with torch.no_grad():  # Disable gradient calculations for inference
-#         predictions = model(input_data)
-    
-#     # If your model returns a scalar value directly:
-#     return float(predictions[0].item())  # Convert the tensor to a Python float
+def call_ai(df: pd.DataFrame):
+    input_data = torch.tensor(df.values, dtype=torch.float32)
+    with torch.no_grad():
+        predictions = model(input_data)
+    return float(predictions[0].item())
 
 def generate_uuidv7():
-    # Obtém o timestamp atual em milissegundos
     timestamp_ms = int(time.time() * 1000)
-    
-    # Converte o timestamp para hexadecimal
     time_hex = f'{timestamp_ms:x}'
-    
-    # Gera a parte aleatória do UUID
     random_hex = ''.join([f'{random.randint(0, 15):x}' for _ in range(26)])
-    
-    # Constrói o UUIDv7 concatenando as partes (versão 7 indica com '7' no início da terceira parte)
     uuidv7 = f'{time_hex[:8]}-{time_hex[8:12]}-7{time_hex[12:15]}-{random_hex[:4]}-{random_hex[4:]}'
-    
     return uuidv7
 
 @app.get("/")
@@ -79,67 +68,59 @@ async def root() -> dict:
 
 @app.post("/mock")
 def mock_data(db: Session = Depends(get_db), num_records: int = 10):
-    record = Prediction(
-        ID=generate_uuidv7(),
-        KNR="4321",
-        Date=(datetime.now() - timedelta(days=random.randint(0, 365))).timestamp(),
-        Feature1=random.uniform(0.0, 100.0),
-        Feature2=random.uniform(0.0, 100.0),
-        Feature3=random.uniform(0.0, 100.0),
-        Prediction_result=1,
-        Real_result=1
-    )
-    
-    db.add(record)
+    for _ in range(num_records):
+        record = Prediction(
+            ID=generate_uuidv7(),
+            KNR="4321",
+            Date=(datetime.now() - timedelta(days=random.randint(0, 365))).strftime('%Y-%m-%d %H:%M:%S'),
+            unique_names=random.uniform(0.0, 100.0),
+            status_10_1=random.uniform(0.0, 100.0),
+            status_10_2=random.uniform(0.0, 100.0),
+            status_10_718=random.uniform(0.0, 100.0),
+            status_13_1=random.uniform(0.0, 100.0),
+            status_13_2=random.uniform(0.0, 100.0),
+            status_13_718=random.uniform(0.0, 100.0),
+            Prediction_result=1,
+            Real_result=1
+        )
+        db.add(record)
     db.commit()
 
     return {"message": f"{num_records} records inserted successfully."}
 
-# @app.post("/predict")
-# async def predict(file: UploadFile, db: Session = Depends(get_db)):
-#     try:
-#         # Step 1: Read the uploaded file into a DataFrame
-#         df = pd.read_csv(file.file)
+@app.post("/predict")
+async def predict(file: UploadFile, db: Session = Depends(get_db)):
+    try:
+        df = pd.read_csv(file.file)
+        expected_columns = ['unique_names', '1_status_10', '2_status_10', '718_status_10',
+                            '1_status_13', '2_status_13', '718_status_13']
+        if list(df.columns) != expected_columns:
+            raise HTTPException(status_code=400, detail=f"File must have the columns: {expected_columns}")
 
-#         # Step 2: Ensure the DataFrame has the correct number of features (adjust as needed)
-#         if len(df.columns) != 10:
-#             raise HTTPException(status_code=400, detail="File must have 10 columns")
+        result = call_ai(df)
 
-#         # Step 3: Call the AI inference function
-#         result = call_ai(df)
+        for _, row in df.iterrows():
+            db_entry = Prediction(
+                ID=generate_uuidv7(),
+                KNR=row['unique_names'],
+                Date=datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                unique_names=row['unique_names'],
+                status_10_1=row['1_status_10'],
+                status_10_2=row['2_status_10'],
+                status_10_718=row['718_status_10'],
+                status_13_1=row['1_status_13'],
+                status_13_2=row['2_status_13'],
+                status_13_718=row['718_status_13'],
+                Prediction_result=int(result),
+                Real_result=random.randint(0, 1)
+            )
+            db.add(db_entry)
+        db.commit()
 
-#         # Step 4: Save the data and prediction to the database
-#         for _, row in df.iterrows():
-#             db_entry = Prediction(
-#                 KNR=row[0],
-#                 Date=row[1],
-#                 Feature1=row[2],
-#                 Feature2=row[3],
-#                 Feature3=row[4],
-#                 Prediction_result=result,
-#                 Real_result=random.randint(0, 1)  # Replace with actual data if needed
-#             )
-#             db.add(db_entry)
-#         db.commit()
+        return {"prediction": result}
 
-#         return {"prediction": result}
-
-    # except Exception as e:
-    #     raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
-
-# To run the app:
-# uvicorn main:app --reload
-
-#-Caso a gente divida em 2 modelos
-# @app.post("/predict_binary")
-# async def predict_binary (file: UploadFile):
-# 	df = pd.read_csv(file.file)
-# 	result = call_ai_binary(df)
-# 	if result is False:
-# 		return {"prediction": result}
-# 	if result:
-# 		result = call_ai_fail(df)
-# 		return {"prediction": result}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
 
 @app.get("/predictions/")
 def read_predictions(skip: int = 0, limit: int = 10, db: Session = Depends(get_db)):
@@ -147,7 +128,7 @@ def read_predictions(skip: int = 0, limit: int = 10, db: Session = Depends(get_d
     return predictions
 
 @app.get("/prediction_id/{ID}")
-def read_prediction(ID :str, db: Session = Depends(get_db)):
+def read_prediction(ID: str, db: Session = Depends(get_db)):
     prediction = db.query(Prediction).filter(Prediction.ID == ID).first()
     if prediction is None:
         raise HTTPException(status_code=404, detail="Prediction not found")
@@ -159,12 +140,10 @@ def update_prediction(ID: str, db: Session = Depends(get_db)):
     if prediction is None:
         raise HTTPException(status_code=404, detail="Prediction not found")
 
-    # Aqui você faria a atualização com novos valores, que podem ser extraídos de algum lugar (ex: request body)
-    # Por exemplo, aqui podemos usar dados fictícios, mas eles deveriam vir de algum lugar válido
-    prediction.feature1 = 2.1
-    prediction.feature2 = 1.234
-    prediction.feature3 = 5.678
-    prediction.prediction_result = 1
+    prediction.unique_names = 2.1  # Substituir com dados reais
+    prediction.status_10_1 = 1.234  # Substituir com dados reais
+    prediction.status_10_2 = 5.678  # Substituir com dados reais
+    prediction.Prediction_result = 1  # Substituir com dados reais
 
     db.commit()
     db.refresh(prediction)
@@ -182,7 +161,7 @@ def delete_prediction(ID: str, db: Session = Depends(get_db)):
 @app.get("/healthcheck/model")
 def healthcheck_model():
     try:
-        test_prediction = 1.0  # Simulação de predição de teste
+        test_prediction = 1.0 
         return {"status": "ok", "prediction": test_prediction}
     except Exception as e:
         return {"status": "error", "message": str(e)}
@@ -198,3 +177,4 @@ def healthcheck_db(db: Session = Depends(get_db)):
 @app.get("/healthcheck/backend")
 def healthcheck_backend():
     return {"status": "ok"}
+
