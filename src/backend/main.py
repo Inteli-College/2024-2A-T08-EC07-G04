49531026902536c2d22ef551845223bf6dcb3c7e
MarioVM <mario.medeiros@sou.inteli.edu.br>
2024-09-02 11:41:02 -0300

    FIX ai function and route Co-authored-by: Rafaela Rojas <Rafaela.Lemos@sou.inteli.edu.com>

--- src/backend/main.py
+++ src/backend/main.py
@@ -10,19 +10,19 @@ import uuid
 import time
 from datetime import datetime, timedelta
 from tensorflow.keras.models import load_model
+import numpy as np
 
 # Configurações do banco de dados
-DATABASE_URL = "postgresql://postgres:SENHA@localhost:5432/fillmore"
+DATABASE_URL = "postgresql://postgres:oFXx2Lqzi2JFbUm@localhost:5432/fillmore"
 engine = create_engine(DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
 
 # Definição do modelo SQLAlchemy para salvar os dados
 class Prediction(Base):
-    __tablename__ = "predictions"
+    __tablename__ = "table"
     ID = Column(String, primary_key=True, index=True)
     KNR = Column(String)
-    Date = Column(String)
     unique_names = Column(Float)
     status_10_1 = Column(Float)
     status_10_2 = Column(Float)
@@ -39,8 +39,7 @@ Base.metadata.create_all(bind=engine)
 app = FastAPI()
 
 #Carregamento do modelo PyTorch
-model = load_model('/model/lmodel.h5')
-model.eval()  # Configura o modelo para o modo de avaliação
+model = load_model('model/model.h5') # Configura o modelo para o modo de avaliação
 
 # Dependência para obter a sessão do DB
 def get_db():
@@ -51,10 +50,20 @@ def get_db():
         db.close()
 
 def call_ai(df: pd.DataFrame):
-    input_data = torch.tensor(df.values, dtype=torch.float32)
-    with torch.no_grad():
-        predictions = model(input_data)
-    return float(predictions[0].item())
+    # Ensure the DataFrame has only the columns expected by the model
+    required_columns = ['unique_names', '1_status_10', '2_status_10', '718_status_10',
+                        '1_status_13', '2_status_13']
+    if not all(col in df.columns for col in required_columns):
+        raise ValueError(f"The input DataFrame does not have the required columns: {required_columns}")
+    
+    input_data = df.values.astype(np.float32)  # Select the correct columns
+    
+    # Ensure the input data has the correct shape
+    input_data = np.reshape(input_data, (input_data.shape[0], input_data.shape[1]))
+
+    predictions = model.predict(input_data)
+    return float(predictions[0])
+
 
 def generate_uuidv7():
     timestamp_ms = int(time.time() * 1000)
@@ -73,7 +82,6 @@ def mock_data(db: Session = Depends(get_db), num_records: int = 10):
         record = Prediction(
             ID=generate_uuidv7(),
             KNR="4321",
-            Date=(datetime.now() - timedelta(days=random.randint(0, 365))).strftime('%Y-%m-%d %H:%M:%S'),
             unique_names=random.uniform(0.0, 100.0),
             status_10_1=random.uniform(0.0, 100.0),
             status_10_2=random.uniform(0.0, 100.0),
@@ -93,22 +101,26 @@ def mock_data(db: Session = Depends(get_db), num_records: int = 10):
 async def predict(file: UploadFile, db: Session = Depends(get_db)):
     try:
         df = pd.read_csv(file.file)
-        expected_columns = ['unique_names', '1_status_10', '2_status_10', '718_status_10',
+        expected_columns = ['KNR','unique_names', '1_status_10', '2_status_10', '718_status_10',
                             '1_status_13', '2_status_13', '718_status_13']
         if list(df.columns) != expected_columns:
+            print(list(df.columns))
             raise HTTPException(status_code=400, detail=f"File must have the columns: {expected_columns}")
         
         #Expected csv = one collum of KNR united, with the collums listed above
 
         #Future FEAT: recive a CSV with various lines of KNR, unite them with pandas and then after call the funtcion call_ai with the treated df
 
+        knr = df['KNR'].iloc[0]
+
+        df = df.drop(columns=['KNR'])
+
         result = call_ai(df)
 
         for _, row in df.iterrows():
             db_entry = Prediction(
                 ID=generate_uuidv7(),
-                KNR=row['unique_names'],
-                Date=datetime.now().strftime('%Y-%m-%d %H:%M:%S'),
+                KNR=knr,
                 unique_names=row['unique_names'],
                 status_10_1=row['1_status_10'],
                 status_10_2=row['2_status_10'],
