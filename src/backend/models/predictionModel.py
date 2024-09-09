b2de357e2ee699db04bd752217d3a91a93647af6
MarioVM <mario.medeiros@sou.inteli.edu.br>
2024-09-09 16:37:45 -0300

    FIX structure database

--- src/backend/models/predictionModel.py
+++ src/backend/models/predictionModel.py
@@ -1,4 +1,4 @@
-from sqlalchemy import create_engine, Column, String, Float, Integer, ForeignKey
+from sqlalchemy import create_engine, Column, String, Float, Integer, ForeignKey, PrimaryKeyConstraint
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import relationship, sessionmaker
 from models.database import Base
@@ -12,25 +12,30 @@ class Prediction(Base):
     Real_result = Column(Integer)
 
     model = relationship('Model')
-    
+    values = relationship('Values', back_populates='prediction')
+
 class Features(Base):
-    __tablename__ = "features"
-    ID = Column(String, ForeignKey('Prediction.ID'), primary_key=True, index=True)
-    ID_modelo = Column(String, ForeignKey('Model.ID_modelo'), index=True) 
-    unique_names = Column(Float)
-    status_10_1 = Column(Float)
-    status_10_2 = Column(Float)
-    status_10_718 = Column(Float)
-    status_13_1 = Column(Float)
-    status_13_2 = Column(Float)
-    status_13_718 = Column(Float)
-
-    model = relationship('Model')
-
-    features = relationship('Features', back_populates='prediction')
+    __tablename__ = "Features"
+    ID_feature = Column(Integer, primary_key=True)
+    name_feature = Column(String)
 
 class Model(Base):
     __tablename__ = "Model"
     ID_modelo = Column(String, primary_key=True, index=True)
     model = Column(String)
     URL_modelo = Column(String)
+
+class Values(Base):
+    __tablename__ = "Values"
+    ID_feature = Column(Integer, ForeignKey('Features.ID_feature'), index=True)
+    ID = Column(String, ForeignKey('Prediction.ID'), index=True)
+    ID_modelo = Column(String, ForeignKey('Model.ID_modelo'), index=True)
+    value_feature = Column(Float)
+
+    __table_args__ = (
+        PrimaryKeyConstraint('ID_feature', 'ID', 'ID_modelo'),
+    )
+
+    feature = relationship('Features')
+    model = relationship('Model')
+    prediction = relationship('Prediction', back_populates='values')
