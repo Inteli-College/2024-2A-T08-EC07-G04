f9e9a6998e610a46f9bebefbb5c2305bf6c6f7ac
lidiamariano <lidia.mariano@sou.inteli.edu.br>
2024-09-12 17:03:25 -0300

    feat: ajuste no back para o database

--- src/backend/controllers/predictionController.py
+++ src/backend/controllers/predictionController.py
@@ -14,12 +14,12 @@ model = load_model('models/model.h5')
 def root():
     return {"message": "Hello World"}
 
-def mock_data(table:str , db: Session = Depends(get_db), num_records: int = 10):
+def mock_data(table: str, db: Session = Depends(get_db), num_records: int = 10):
     for _ in range(num_records):
         if table == 'Model':
             record = Model(
-                ID_modelo=1,
-                model='progression_V1',
+                ID_modelo=generate_uuidv7(),
+                model='sequencial_V1',
                 URL_modelo="http://example.com/model_1"
             )
         db.add(record)
@@ -40,21 +40,41 @@ async def predict(file: UploadFile, db: Session = Depends(get_db)):
 
         result = call_ai(df)
 
+        prediction_id = generate_uuidv7()
         for _, row in df.iterrows():
-            db_entry = Prediction(
-                ID=generate_uuidv7(),
+            prediction_entry = Prediction(
+                ID=prediction_id,
                 KNR=knr,
-                unique_names=row['unique_names'],
-                status_10_1=row['1_status_10'],
-                status_10_2=row['2_status_10'],
-                status_10_718=row['718_status_10'],
-                status_13_1=row['1_status_13'],
-                status_13_2=row['2_status_13'],
-                status_13_718=row['718_status_13'],
+                ID_modelo="1", 
                 Prediction_result=int(result),
                 Real_result=random.randint(0, 1)
             )
-            db.add(db_entry)
+            db.add(prediction_entry)
+
+            features = [
+                ('1_status_10', row['1_status_10']),
+                ('2_status_10', row['2_status_10']),
+                ('718_status_10', row['718_status_10']),
+                ('1_status_13', row['1_status_13']),
+                ('2_status_13', row['2_status_13']),
+                ('718_status_13', row['718_status_13']),
+            ]
+
+            for feature_name, feature_value in features:
+                feature = db.query(Features).filter(Features.name_feature == feature_name).first()
+                if not feature:
+                    feature = Features(name_feature=feature_name)
+                    db.add(feature)
+                    db.commit()  
+
+                values_entry = Values(
+                    ID_feature=feature.ID_feature,
+                    ID=prediction_id,
+                    ID_modelo="1",  
+                    value_feature=feature_value
+                )
+                db.add(values_entry)
+
         db.commit()
 
         return {"prediction": result}
@@ -63,7 +83,7 @@ async def predict(file: UploadFile, db: Session = Depends(get_db)):
         raise HTTPException(status_code=500, detail=f"An error occurred: {str(e)}")
 
 def read_predictions(table: str, skip: int, limit: int, db: Session = Depends(get_db)) -> List[dict]:
-    # Map table names to their corresponding SQLAlchemy model classes
+
     table_map = {
         'Prediction': Prediction,
         'Features': Features,
@@ -71,20 +91,15 @@ def read_predictions(table: str, skip: int, limit: int, db: Session = Depends(ge
         'Values': Values
     }
     
-    # Check if the table exists in the table map
     if table not in table_map:
         raise HTTPException(status_code=400, detail=f"Table '{table}' not recognized.")
     
-    # Get the model class corresponding to the table name
     model_class = table_map[table]
     
-    # Query the database for the specified table
     records = db.query(model_class).offset(skip).limit(limit).all()
     
-    # Convert the SQLAlchemy objects to dictionaries for better display
     result = [record.__dict__ for record in records]
     
-    # Remove the SQLAlchemy internal state from the dictionary
     for record in result:
         record.pop('_sa_instance_state', None)
     
@@ -101,9 +116,7 @@ def update_prediction(ID: str, db: Session = Depends(get_db)):
     if prediction is None:
         raise HTTPException(status_code=404, detail="Prediction not found")
 
-    prediction.unique_names = 2.1
-    prediction.status_10_1 = 1.234
-    prediction.status_10_2 = 5.678
+    prediction.KNR = "Updated KNR"
     prediction.Prediction_result = 1
     
     db.commit()
