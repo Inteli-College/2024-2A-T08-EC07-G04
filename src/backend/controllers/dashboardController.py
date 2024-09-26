36fa6dddad97fa709c80bdb28459b496d19d2d01
olin-med <olin.costa@sou.inteli.edu.br>
2024-09-26 17:44:44 -0300

    feat: adds prediction mock

--- src/backend/controllers/dashboardController.py
+++ src/backend/controllers/dashboardController.py
@@ -7,10 +7,14 @@ from models.database import get_db
 from typing import List
 
 def get_timestamp_from_uuid(uuid_str: str) -> datetime:
-    # Extract the timestamp part from the UUID and convert it to a datetime object
-    uuid_obj = uuid.UUID(uuid_str)
-    timestamp = uuid_obj.time / 1e7 - 12219292800  # Convert UUID time to UNIX timestamp
-    return datetime.utcfromtimestamp(timestamp)
+    try:
+        # Extract the timestamp part from the UUID and convert it to a datetime object
+        uuid_obj = uuid.UUID(uuid_str)
+        timestamp = uuid_obj.time / 1e7 - 12219292800  # Convert UUID time to UNIX timestamp
+        return datetime.utcfromtimestamp(timestamp)
+    except ValueError:
+        # Handle invalid UUID strings
+        return None
 
 def read_predictions_current_week(skip: int, limit: int, db: Session = Depends(get_db)) -> List[dict]:
     # Calculate the start of the current week (Monday at 00:00)
@@ -21,12 +25,12 @@ def read_predictions_current_week(skip: int, limit: int, db: Session = Depends(g
     records = db.query(Prediction).offset(skip).limit(limit).all()
     
     # Filter records that are within the current week based on the UUID timestamp
-    result = [
-        record.__dict__ for record in records 
-        if get_timestamp_from_uuid(record.ID) >= start_of_week
-    ]
-    
-    for record in result:
-        record.pop('_sa_instance_state', None)
+    result = []
+    for record in records:
+        timestamp = get_timestamp_from_uuid(record.ID)
+        if timestamp and timestamp >= start_of_week:
+            record_dict = record.__dict__
+            record_dict.pop('_sa_instance_state', None)
+            result.append(record_dict)
     
     return result
