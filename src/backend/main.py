e3a5eb3db33d80fef3b4012302c65406e55c6a71
olin-med <olin.costa@sou.inteli.edu.br>
2024-09-27 11:20:12 -0300

    fix: fixes dbeaver conectivity

--- src/backend/main.py
+++ src/backend/main.py
@@ -1,11 +1,21 @@
 from fastapi import FastAPI
 from models.database import engine, Base
 from routes import predictionRoutes, healthChecksRoutes, dashboardRoutes
+from fastapi.middleware.cors import CORSMiddleware
 
 # Criação da tabela no banco de dados
 Base.metadata.create_all(bind=engine)
 
 app = FastAPI()
+
+# Allow all origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # This allows all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # This allows all HTTP methods (GET, POST, PUT, etc.)
+    allow_headers=["*"],  # This allows all headers
+)
 
 # Registro das rotas
 app.include_router(predictionRoutes.router)
